2bf2f6d10b97decd23cfd96b495019ee41e2dbad
Daniil <bombyascher@gmail.com>
2024-04-15 18:56:32 +0300

    Add CRUD for page model. Implements lombok.

--- src/main/java/io/spring/Application.java
+++ src/main/java/io/spring/Application.java
@@ -1,13 +1,18 @@
 package io.spring;
 
+import io.spring.model.Page;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 @RestController
 public class Application {
+    private List<Page> pages = new ArrayList<>();
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -15,5 +20,42 @@ public class Application {
     @GetMapping("/")
     String home() {
         return "Hello World!!!!";
+    }
+
+    @GetMapping("/pages")
+    List<Page> index(@RequestParam(defaultValue = "10") Integer limit) {
+        return pages.stream().limit(limit).toList();
+    }
+
+    @PostMapping("/pages")
+    Page create(@RequestBody Page page) {
+        pages.add(page);
+        return page;
+    }
+
+    @GetMapping("/pages/{id}")
+    Optional<Page> show(@PathVariable String id) {
+        return pages.stream()
+                .filter(page -> page.getSlug().equals(id))
+                .findFirst();
+    }
+
+    @PutMapping("/pages/{id}")
+    Page update(@PathVariable String id, @RequestBody Page data) {
+        var optionalPage = pages.stream()
+                .filter(p -> p.getSlug().equals(id))
+                .findFirst();
+        if (optionalPage.isPresent()) {
+            var page = optionalPage.get();
+            page.setName(data.getName());
+            page.setSlug(data.getSlug());
+            page.setBody(data.getBody());
+        }
+        return data;
+    }
+
+    @DeleteMapping("/pages/{id}")
+    void delete(@PathVariable String id) {
+        pages.removeIf(page -> page.getSlug().equals(id));
     }
 }
---
+++ src/main/java/io/spring/model/Page.java
@@ -0,0 +1,14 @@
+package io.spring.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Page {
+    private String slug;
+    private String name;
+    private String body;
+}
