d8614476578df9cd675a5380929b808cb9cca9e6
Daniil <bombyascher@gmail.com>
2024-04-17 18:07:31 +0300

    Add checkstyle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    id("checkstyle")
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.3"
     id("io.freefair.lombok") version "8.6"
