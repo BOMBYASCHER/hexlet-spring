6d7fbaa9bfd263ea3ffe82272d0975266d42ade8
Daniil <bombyascher@gmail.com>
2024-04-17 17:56:28 +0300

    Implementation H2 database and Spring Boot JPA

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-devtools")
-
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
